4fe4d3461bbe48d2f515d68958342ba9daee03bc
ayutac <fly.high.android@gmail.com>
2024-01-21 15:09:11 +0100

    Added ordering the cards.

--- src/main/java/org/abos/cards/examples/classic/skat/SkatDealingOption.java
+++ src/main/java/org/abos/cards/examples/classic/skat/SkatDealingOption.java
@@ -1,18 +1,21 @@
 package org.abos.cards.examples.classic.skat;
 
 import org.abos.cards.core.Option;
+import org.abos.cards.core.actions.SortAction;
 import org.abos.cards.core.simple.SimpleCard;
 import org.abos.cards.core.SubGame;
 import org.abos.cards.core.actions.Action;
 import org.abos.cards.core.actions.MoveAction;
 import org.abos.cards.core.actions.MoveType;
 import org.abos.cards.core.actions.ShuffleAction;
+import org.abos.cards.examples.classic.FrenchType;
 
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.function.Predicate;
 
-public class SkatDealingOption implements Option<SimpleCard> {
+public final class SkatDealingOption implements Option<SimpleCard> {
 
     private SkatSubGame subGame;
 
@@ -50,6 +53,7 @@ public class SkatDealingOption implements Option<SimpleCard> {
         final List<Action<SimpleCard>> actions = new LinkedList<>();
         actions.add(new ShuffleAction<>(subGame, SkatBoard.DECK_STACK));
         final SkatPlayer[] dealingOrder = subGame.getDealingPlayer().dealingOrder();
+        // deal cards
         for (SkatPlayer player : dealingOrder) {
             actions.add(new MoveAction<>(subGame, SkatBoard.DECK_STACK, SkatBoard.HAND_STACKS.get(player), MoveType.TOP, 3));
         }
@@ -59,6 +63,11 @@ public class SkatDealingOption implements Option<SimpleCard> {
         }
         for (SkatPlayer player : dealingOrder) {
             actions.add(new MoveAction<>(subGame, SkatBoard.DECK_STACK, SkatBoard.HAND_STACKS.get(player), MoveType.TOP, 3));
+        }
+        // sort dealt cards
+        actions.add(new SortAction<>(subGame, SkatBoard.SKAT_STACK, Comparator.comparing(card -> (FrenchType)card.getType(), SkatComparator.byTrump(SkatTrump.JACK))));
+        for (SkatPlayer player : dealingOrder) {
+            actions.add(new SortAction<>(subGame, SkatBoard.HAND_STACKS.get(player), Comparator.comparing(card -> (FrenchType)card.getType(), SkatComparator.byTrump(SkatTrump.JACK))));
         }
         return actions;
     }
